73e01da2732eb19ad6a1142f6ea04b0d697a8343
Martín Calderón <elmcwl@gmail.com>
2024-03-11 00:31:36 -0700

    feat: metodos y atributos estaticos

--- 12.php
+++ 12.php
@@ -1,2 +1,49 @@
 <?php
 include 'includes/header.php';
+
+// static
+class Empleado {
+  protected static $nombre;
+  public $apellido;
+  public $departamento;
+  public $email;
+  public $coidigo;
+  
+  public function __construct(string $nombre, string $apellido, string $departamento, string $email, int $coidigo)
+  {
+    self::$nombre = $nombre;
+    $this->apellido = $apellido;
+    $this->departamento = $departamento;
+    $this->email = $email;
+    $this->coidigo = $coidigo;
+  }
+    
+  public function obtenerNombre()
+  {
+    return $this->nombre . '<br>';
+  }
+  public function cambiarNombre($nombre)
+  {
+    $this->nombre = $nombre;
+  }
+  
+  public static function getEmpleado()
+  {
+    echo "Desde método estático";
+  }
+  
+  public static function getNombre()
+  {
+    return self::$nombre;
+  }
+}
+
+Empleado::getEmpleado();
+
+$juan = new Empleado('Juan', 'De la Torre', 'TI', 'Lp5p4@example.com', 006);
+echo '<br>';
+echo $juan::getNombre();
+
+echo '<pre>';
+var_dump($juan);
+echo '</pre><br>';
