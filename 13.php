24c7c4d7aef4e6c3dc0b96ece789b9496025047a
Martín Calderón <elmcwl@gmail.com>
2024-03-11 01:31:36 -0700

    feat: Comunicar 2 clases

--- 13.php
+++ 13.php
@@ -1,2 +1,43 @@
 <?php
 include 'includes/header.php';
+
+include './DB.php';
+
+
+// comunicar 2 clases 
+class Empleado {
+  protected $nombre;
+  public $apellido;
+  public $departamento;
+  public $email;
+  public $coidigo;
+  
+  public function __construct(string $nombre, string $apellido, string $departamento, string $email, int $coidigo)
+  {
+    $this->nombre = $nombre;
+    $this->apellido = $apellido;
+    $this->departamento = $departamento;
+    $this->email = $email;
+    $this->coidigo = $coidigo;
+  }
+  
+  public function getNombre()
+  {
+    return $this->nombre . '<br>';
+  }
+  public function setNombre($nombre)
+  {
+    $this->nombre = $nombre;
+  }
+}
+
+$juan = new Empleado('Juan', 'De la Torre', 'TI', 'Lp5p4@example.com', 006);
+
+$nombre = $juan->getNombre();
+echo $nombre;
+
+$db = new DB($nombre);
+
+var_dump($db);
+echo '<br>';
+$db->guardar();
